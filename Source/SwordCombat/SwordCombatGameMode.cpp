b5bc1bb091e4fff4250e1c7912685da0d2fa0ebf
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-09-23 13:05:37 +0300

    Initial commit

---
+++ Source/SwordCombat/SwordCombatGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "SwordCombatGameMode.h"
+#include "SwordCombatCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ASwordCombatGameMode::ASwordCombatGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
