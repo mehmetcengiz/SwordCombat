a4f6eef073daf87f4e8f47585a6bfb93503c2c7a
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-09-30 16:12:26 +0300

    CharacterStateTrigger created.

---
+++ Source/SwordCombat/WorldProps/CharacterStateTrigger.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CharacterStateTrigger.generated.h"
+
+UENUM()
+namespace ECharacterState {
+	enum Type {
+		INTERACTSTATE               UMETA(DisplayName = "Interact State"),
+		COMBATSTATE                 UMETA(DisplayName = "Combat State"),
+	};
+}
+
+UCLASS()
+class SWORDCOMBAT_API ACharacterStateTrigger : public AActor {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACharacterStateTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character State")
+	TEnumAsByte<ECharacterState::Type> CharacterState;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+};
