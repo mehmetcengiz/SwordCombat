79022ca02096c77137028073856184e58c6e3e10
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-10-10 16:10:05 +0300

    Player has time to enable next combo attack. Enabling implemented. Combo attack not implemented.

--- Source/SwordCombat/Weapons/TwinBlade/TwinBlade.cpp
+++ Source/SwordCombat/Weapons/TwinBlade/TwinBlade.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TwinBlade.h"
+#include "TimerManager.h"
+#include "Engine/Engine.h" // For debug delete later.
+
 
 void ATwinBlade::BeginPlay(){
 	Super::BeginPlay();
@@ -10,11 +13,38 @@ void ATwinBlade::OnPrimaryAttack(){
 	Super::OnPrimaryAttack();
 	UE_LOG(LogTemp, Warning, TEXT("ATwinBlade >> OnPrimaryAttack"));
 	
+
 	if (CombatCharacter->IsReadyToAttack()){
+		
+		//Play montage and set character is ready to attacking to false. 
 		float MontageTime = CombatCharacter->GetMesh()->GetAnimInstance()->Montage_Play(Attack2, 1.0f, EMontagePlayReturnType::MontageLength, 0);
 		CombatCharacter->DisableAttackingForCertainTime(MontageTime / 2);
+
+		//Set min max times for combo trigger. 
+		NextComboMinTime = MontageTime / 4;
+		NextComboMaxTime = NextComboMinTime + MontageTime / 2;
+
+		//Execute saving combo trigger and reseting combo by times.
+		FTimerHandle Handle;
+		GetWorld()->GetTimerManager().SetTimer(OUT Handle, this, &ATwinBlade::EnableSaveCombo, NextComboMinTime, false);
+		FTimerHandle Handle2;
+		GetWorld()->GetTimerManager().SetTimer(OUT Handle2, this, &ATwinBlade::ResetCombo, NextComboMaxTime, false);
+	
+	} else if(bSaveCombo){
+		SaveCombo();
+		bSaveCombo = false;
 	}
+}
 
+void ATwinBlade::SaveCombo(){
+	//TODO save combo;
+	GEngine->AddOnScreenDebugMessage(-1, 555.f, FColor::Green, "Next Combo Triggered.");
+}
 
-
+void ATwinBlade::ResetCombo() {
+	if(bSaveCombo){
+		GEngine->AddOnScreenDebugMessage(-1, 555.f, FColor::Red, "Combo Reseted");
+		bSaveCombo = false;
+		//Reset combo.
+	}
 }
--- Source/SwordCombat/Weapons/TwinBlade/TwinBlade.h
+++ Source/SwordCombat/Weapons/TwinBlade/TwinBlade.h
@@ -22,8 +22,23 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "Animations")
 	UAnimMontage* Attack2;
 
+private:
+	/**Min time for player to press attack again to execute next combo.*/
+	UPROPERTY()
+	float NextComboMinTime = 0.5f;
+	/**Max time for player to press attack again to execute next combo.*/
+	UPROPERTY()
+	float NextComboMaxTime = 1.0f;
 
+	bool bSaveCombo = false;
+
+	void EnableSaveCombo()	{ bSaveCombo = true;	}
+	void SaveCombo();
+	void ResetCombo();
 	
+
+
+protected:	
 	void BeginPlay() override;
 	void OnPrimaryAttack() override;
 
