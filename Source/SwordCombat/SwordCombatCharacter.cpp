31789130c66e10f0c4549ccf7a9fe9f7a781bf71
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-10-18 01:52:56 +0300

    Revert "Cleaning up from animation blueprint."
    This reverts commit 8728ef2e3c50032917bc5701a7338deb092320d0.

--- Source/SwordCombat/SwordCombatCharacter.cpp
+++ Source/SwordCombat/SwordCombatCharacter.cpp
@@ -221,21 +221,16 @@ void ASwordCombatCharacter::PutSwordBackToSheath(){
 	}
 }
 
-
 void ASwordCombatCharacter::TakeHit(float Damage) {
 	UE_LOG(LogTemp, Warning, TEXT("I tooked hit !!!"));
 	//TODO Play Animation. 
-
 	bGotHit = true;
-	DisableFromAttackingForCertainTime(1);
+	bIsReadyToAttack = false;
+	FTimerHandle Handle;
+	GetWorld()->GetTimerManager().SetTimer(OUT Handle, this, &ASwordCombatCharacter::ResetCharacter, HitClipTime, false);
 	//TODO Apply damage.
 }
 
-void ASwordCombatCharacter::DisableFromAttackingForCertainTime(float TimeToDisable) {
-	bIsReadyToAttack = false;
-	FTimerHandle Handle;
-	GetWorld()->GetTimerManager().SetTimer(OUT Handle, this, &ASwordCombatCharacter::ResetCharacter, TimeToDisable, false);
-}
 
 void ASwordCombatCharacter::ResetCharacter() {
 	bGotHit = false;
--- Source/SwordCombat/SwordCombatCharacter.h
+++ Source/SwordCombat/SwordCombatCharacter.h
@@ -102,11 +102,11 @@ public:
 	bool IsReadyToAttack() { return bIsReadyToAttack; }
 	void TakeHit(float Damage);
 	void PutSwordBackToSheath();
-	void DisableFromAttackingForCertainTime(float TimeToDisable);
 	void ResetCharacter();
 
 	UPROPERTY(BlueprintReadWrite,Category="Animations")
 	bool bGotHit = false;
+	float HitClipTime = 1.16f;
 
 private:
 	ECharacterState CurrentCharacterState = ECharacterState::INTERACT;
